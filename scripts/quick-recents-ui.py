753e4f5278dfada111fb54cd95def6e4c63f11fd
w-e-w <40751091+w-e-w@users.noreply.github.com>
2024-10-23 18:49:37 +0900

    making update parameters work

--- scripts/quick-recents-ui.py
+++ scripts/quick-recents-ui.py
@@ -1,34 +1,62 @@
-import modules.scripts as scripts
 import modules.infotext_utils as parameters_copypaste
+from modules import shared, errors
+import modules.scripts as scripts
+from functools import lru_cache
+from heapq import nlargest
+from PIL import Image
 import gradio as gr
 import os
-from heapq import nlargest
-from pathlib import Path
-from modules import shared, errors
-from PIL import Image
-from functools import lru_cache
+
+allowed_ext = ('.png', '.jpg', '.jpeg', '.webp', '.avif')
 
 
 @lru_cache(maxsize=128)
-def read_image(img_path):
-    with Image.open(img_path) as img:
-        img.already_saved_as = img_path
-        return img
+def create_fake_image(img_path):
+    fake_image = Image.new(mode="RGB", size=(1, 1))
+    fake_image.already_saved_as = img_path
+    return fake_image
 
 
-def read_images(img_paths):
-    images = []
-    for img in img_paths:
-        try:
-            images.append(read_image(img))
-        except Exception:
-            errors.report(f"Error reading image {img}", exc_info=True)
-    return images
+def scan_images(dir_path):
+    for root, _, fns in os.walk(dir_path):
+        for fn in fns:
+            if fn.lower().endswith(allowed_ext):
+                yield os.path.join(root, fn)
+
+
+def get_recent_images(n, is_img2img):
+    # Crawl through txt/img2img directories to find recent images
+    img_dir = shared.opts.outdir_samples or (shared.opts.outdir_img2img_samples if is_img2img else shared.opts.outdir_txt2img_samples)
+    return nlargest(n, scan_images(img_dir), key=os.path.getmtime)
+
+
+def get_gallery_images(n, is_img2img):
+    return [create_fake_image(img_path) for img_path in get_recent_images(n, is_img2img)]
+
+
+def update_params(evt: gr.SelectData, images):
+    try:
+        image, q, timestamp = images[evt.index]['name'].rpartition('?')
+        image_path = image if q else timestamp
+        with Image.open(image_path) as img:
+            metadata = img.info
+        return metadata.get('parameters', '')
+    except Exception:
+        errors.report("Error reading image parameters", exc_info=True)
+        return ''
 
 
 class QuickRecentsScript(scripts.Script):
     # So the tap appears at the top
     sorting_priority = -5
+
+    def __init__(self):
+        super().__init__()
+        self.recent_images = []
+        self.num_img = 8
+        self.num_cols = 2
+        self.num_rows = (self.num_img + self.num_cols - 1) // self.num_cols
+        self.is_img2img = None
 
     def title(self):
         return "Quick Recents"
@@ -37,25 +65,7 @@ class QuickRecentsScript(scripts.Script):
         return scripts.AlwaysVisible
 
     def ui(self, is_img2img):
-        # Crawl through txt/img2img directories to find recent images
-        def get_recent(n):
-            txt2img_dir = shared.opts.outdir_img2img_samples if is_img2img else shared.opts.outdir_txt2img_samples
-            png_files = (str(Path(root) / file) for root, _, files in os.walk(txt2img_dir) for file in files if file.endswith('.png'))
-            recent_files = nlargest(n, png_files, key=os.path.getmtime)
-            return recent_files
-
-        num_img = 8
-        num_cols = 2
-        num_rows = (num_img + num_cols - 1) // num_cols
-
-        # Get Paraminfo of image through metadata
-        def update_params(evt: gr.SelectData):
-            imgpat = evt.value['image']['path']
-            with Image.open(imgpat) as img:
-                metadata = img.info
-            geninfo = metadata.get("parameters")
-            return geninfo
-
+        self.is_img2img = is_img2img
         generation_info = gr.Textbox(visible=False)
 
         with gr.Accordion('Quick Recents', open=False):
@@ -69,14 +79,18 @@ class QuickRecentsScript(scripts.Script):
                 refresh = gr.Button('\U0001f504', scale=1)
 
             gallery = gr.Gallery(
-                value=read_images(get_recent(num_img)),
+                value=get_gallery_images(self.num_img, is_img2img),
                 show_label=False,
-                columns=num_cols,
-                rows=num_rows,
+                columns=self.num_cols,
+                rows=self.num_rows,
                 object_fit='contain',
                 allow_preview=False,
                 format='png',
             )
 
-            gallery.select(update_params, outputs=generation_info)
-            refresh.click(lambda: read_images(get_recent(num_img)), outputs=gallery)
+            gallery.select(
+                fn=update_params,
+                inputs=gallery,
+                outputs=generation_info
+            )
+            refresh.click(lambda: get_gallery_images(self.num_img, is_img2img), outputs=gallery)
