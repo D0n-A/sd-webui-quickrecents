ec51dafb69b2f7ec5d49e0b0afa09f82b7969ec9
w-e-w <40751091+w-e-w@users.noreply.github.com>
2024-10-23 19:43:46 +0900

    allow total Image to be dynamically adjustable
    comments and clean up

--- scripts/quick-recents-ui.py
+++ scripts/quick-recents-ui.py
@@ -11,18 +11,20 @@ allowed_ext = ('.png', '.jpg', '.jpeg', '.webp', '.avif')
 
 shared.options_templates.update(shared.options_section(('quick_recent', 'Quick recent'), {
     'quick_recent_total_recent_img': shared.OptionInfo(
-        8, 'Total recent Images to show in gallery',
-        gr.Number, {'minimum': 1, 'maximum': 50}
-    ).needs_reload_ui(),
+        8, 'Total recent Images to show in quick recent gallery',
+        gr.Number, {'minimum': 1, 'maximum': 1000, 'precision': 0}
+    ),
     'quick_recent_img_per_row': shared.OptionInfo(
-        2, 'Image per row in gallery',
-        gr.Number, {'minimum': 1, 'maximum': 50}
+        2, 'Image per row in quick recent gallery',
+        gr.Number, {'minimum': 1, 'maximum': 50, 'precision': 0}
     ).needs_reload_ui(),
 }))
 
 
-@lru_cache(maxsize=128)
+@lru_cache(maxsize=2048)
 def create_fake_image(img_path):
+    # create a fake image of 1x1 to send to gradio
+    # Image.already_saved_as is a custom attribute of Webui to prevent gradio temp file creation
     fake_image = Image.new(mode="RGB", size=(1, 1))
     fake_image.already_saved_as = img_path
     return fake_image
@@ -37,15 +39,19 @@ def scan_images(dir_path):
 
 def get_recent_images(n, is_img2img):
     # Crawl through txt/img2img directories to find recent images
+    # if specified webui will output to outdir_samples first before using txt/img2img's own directories
     img_dir = shared.opts.outdir_samples or (shared.opts.outdir_img2img_samples if is_img2img else shared.opts.outdir_txt2img_samples)
     return nlargest(n, scan_images(img_dir), key=os.path.getmtime)
 
 
-def get_gallery_images(n, is_img2img):
-    return [create_fake_image(img_path) for img_path in get_recent_images(n, is_img2img)]
+def get_gallery_images(is_img2img):
+    return [create_fake_image(img_path) for img_path in get_recent_images(shared.opts.quick_recent_total_recent_img, is_img2img)]
 
 
 def update_params(evt: gr.SelectData, images):
+    # index of the image in the gallery from evt
+    # images is the list of images in the gallery from the webpage
+    # image_path contains separator '?', needs to be removed
     try:
         image, q, timestamp = images[evt.index]['name'].rpartition('?')
         image_path = image if q else timestamp
@@ -66,7 +72,6 @@ class QuickRecentsScript(scripts.Script):
         self.recent_images = []
         self.num_img = shared.opts.quick_recent_total_recent_img
         self.num_cols = shared.opts.quick_recent_img_per_row
-        self.num_rows = (self.num_img + self.num_cols - 1) // self.num_cols
 
     def title(self):
         return "Quick Recents"
@@ -92,7 +97,6 @@ class QuickRecentsScript(scripts.Script):
                     value=None,
                     show_label=False,
                     columns=self.num_cols,
-                    rows=self.num_rows,
                     object_fit='contain',
                     allow_preview=False,
                     format='png',
@@ -103,5 +107,5 @@ class QuickRecentsScript(scripts.Script):
                     inputs=gallery,
                     outputs=generation_info
                 )
-                refresh.click(lambda: get_gallery_images(self.num_img, is_img2img), outputs=[gallery])
-                block.load(lambda: get_gallery_images(self.num_img, is_img2img), outputs=[gallery])
+                refresh.click(lambda: get_gallery_images(is_img2img), outputs=[gallery])
+                block.load(lambda: get_gallery_images(is_img2img), outputs=[gallery])
